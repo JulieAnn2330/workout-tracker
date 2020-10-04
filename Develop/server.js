494fd4dfef9c63ca91f8be938568e63cfb14c705
Julie Schaub <julie.schaub@hotmail.com>
2020-10-04 10:51:13 -0500

    added server.js file and pseudocode

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+/* Pseudocode:
+1. Create workout tracker for user
+2. Technology needed to be installed:
+    a. MonogoDB
+    b. Mongoose
+    c. Express
+    d. Nodemon
+    e. npm i
+    f. Atlas DB
+3. Create
+    a. Daily Workouts and track them
+    b. log multiple workouts on a given day
+    c. track the name, type, weight, sets, reps and duration of exercise
+    d. if exercise is cardio, track distanch
+4. Acceptance criteria
+    a. When user loads page they should be able to create a new workout or continue with the last workout
+    b. Add exercises to a previous workout plan
+    c. Add new exercises to a new workout plan
+    d. View the combined weight of multiple exercises on the stats page
+    e. Deploy application with Heroku using MongoDB Atlas
+    */
+
