232c6352b1dad7ca921aaaf681f812c91d2f93b0
Julie Schaub <julie.schaub@hotmail.com>
2020-10-07 14:00:29 -0500

    moved server file

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+  
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
-const apiRoutes = require("./Develop/routes/apiRoutes");
-const htmlRoutes = require("./Develop/routes/htmlRoutes")
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(morgan("dev"));
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+}
+);
 
-app.use(htmlRoutes);
-app.use(apiRoutes);
+const db = require("./Develop/models");
+
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
